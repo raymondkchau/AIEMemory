df947f6b624ce58214780c02d2e3b4069a9a27b5
Raymond Chau <raymondkietchau@gmail.com>
2017-09-13 12:03:52 -0700

    Added 05-FileIO, finished most of it.

---
+++ 05-FileIO/main.cpp
@@ -0,0 +1,188 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <random>
+#include <time.h>
+
+int closed1()
+{
+	std::string fileName;
+	std::cout << "Howdy! ";
+	while (true)
+	{
+		while (true)
+		{
+			std::cout << "What file would you like to read?" << std::endl;
+			std::cin >> fileName;
+
+			std::fstream file;
+			file.open(fileName, std::ios_base::in);
+
+			if (file.fail())
+			{
+				std::cout << "Error: File not found" << std::endl;
+				std::cout << std::endl;
+				break;
+			}
+
+			std::cout << std::endl;
+			std::cout << fileName << std::endl;
+			std::cout << "----------------" << std::endl;
+
+			std::string buffer;
+			while (std::getline(file, buffer))
+			{
+				std::cout << buffer << std::endl;
+			}
+
+			std::cout << std::endl;
+
+			file.clear();
+			file.flush();
+			file.close();
+		}
+	}
+
+	return 0;
+}
+
+int closed2()
+{
+	std::string fileName;
+	std::cout << "Where would you like to save this?" << std::endl;
+	std::cin >> fileName;
+
+	std::fstream file;
+	file.open(fileName, std::ios_base::out);
+
+	if (file.fail())
+	{
+		std::cout << "Error: File not found" << std::endl;
+		std::cout << std::endl;
+		return -1;
+	}
+			
+	file.seekp(0, std::ios_base::end);
+
+	std::cout << "What's your first name?" << std::endl;
+	std::cin >> fileName;
+	file << "Name: " << fileName << std::endl;
+
+	std::cout << "How old are you?" << std::endl;
+	std::cin >> fileName;
+	file << "Age: " << fileName << std::endl;
+
+	std::cout << "What's your favorite color?" << std::endl;
+	std::cin >> fileName;
+	file << "Favorite Color: " << fileName << std::endl;
+
+	std::cout << "Thanks! Check your file for your results." << std::endl;
+
+	file.clear();
+	file.flush();
+	file.close();
+
+	return 0;
+}
+
+void open1()
+{
+	char * text[5];
+	text[0] = { "September 13th, 2017" };
+	text[1] = { "--------------------" };
+	text[2] = { "These are the last minutes of my life." };
+	text[3] = { "The dinosaurs will soon break in." };
+	text[4] = { "They can smell my hotdogs." };
+
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < std::strlen(text[i]); j++)
+		{
+			srand(time(NULL));
+			int RNG = rand() % 10;
+
+			if (RNG <= 2)
+			{
+				char change[128];
+				//gonna assume it works since I can't exactly check. Won't let me run strcpy.
+				std::strcpy(text[i], change);
+				change[j] = '█';
+				std::strcpy(change, text[i]);
+			}
+		}
+	}
+
+	std::fstream file;
+	file.open("importantMessage.txt", std::ios_base::out);
+
+	if (file.fail())
+	{
+		std::cout << "Error: File not found." << std::endl;
+	}
+
+	file.seekp(0, std::ios_base::end);
+
+	
+	//█ alt+219 = Block, \219 maybe?
+	file << text[0] << std::endl;
+	file << text[1] << std::endl;
+	file << std::endl;
+	file << text[2] << std::endl;
+	file << text[3] << std::endl;
+	file << text[4] << std::endl;
+}
+
+int main()
+{
+	/*
+	std::fstream file;
+
+	file.open("text.txt", std::ios_base::out);
+
+	if (file.fail())
+	{
+		std::cout << "File not found." << std::endl;
+		return -1;
+	}
+
+	//Reading from a file
+	std::string buffer;
+	while (std::getline(file, buffer)) //iterates until error or end of file
+	{
+		//print the line after its buffered
+		std::cout << buffer << std::endl;
+	}
+
+	file.clear(); // reset the error flags, such as EOF
+
+	file.seekp(0, std::ios_base::end); //move my cursor to the end for writing
+
+	//Writing to a file
+	//Write a message!
+	//Writes a new line and a message to the text file
+	file << std::endl << "Today is always the present! :)";
+
+	file.flush(); //flush the output buffer, making sure all outputs are sent
+	file.close(); //close and release the file
+
+	//let's make another thingie
+
+	std::fstream autoCreate;
+
+	autoCreate.open("createme.text", std::ios_base::out | std::ios_base::in);
+
+	if (autoCreate.fail())
+	{
+		std::cout << "Something went wrong." << std::endl;
+		return -1;
+	}
+	
+	autoCreate.close();
+	*/
+
+	//closed1();
+	//closed2();
+	open1();
+
+	while (true); // Keeps the program going
+}
